d504beaa0c9610d08132f217ad28f2567ac818c1
Dean Pierce <pierce403@gmail.com>
2020-08-13 01:24:32 -0700

    parsing is hard

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ function deposit()
     console.log("sending GNT to deposit address");
 
     // send over the GNT to the deposit address
-    gntContract.transfer(depositAddress,ethers.constants.WeiPerEther*depositAmount).then(function (value) {
+    gntContract.transfer(depositAddress,web3.toWei(depositAmount)).then(function (value) {
     //gntContract.transfer(depositAddress,"3000000000000000000").then(function (value) {
         console.log("deposit complete "+value);
     })
